212a52f7b6e891d72f4d11c0abd1624770c33ae2
Dongchu Xia <dongchuxia@tudelft.nl>
2026-07-22 16:53:21 +0200

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,1 +1,25 @@
+cosnt mongoose = require('mongoose');
 
+const contactSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
