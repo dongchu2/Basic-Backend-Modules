6b6cb29c113f673eeeba7497629248e244562a17
Dongchu Xia <dongchuxia@tudelft.nl>
2026-07-22 16:49:42 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    origin:"*",
+  })
+  );
+
+app.use(express.json();
+
+app.get("/",(reg,res)=>{
+  res.send("backend is running");
+});
+
+app.use("/api/contact",contactRoutes);
+const PORT = process.env.PORT || 3001;
+
+mongoose
+        .connect(process.env.MONGODB_URL)
+        .then(()=>{
+          console.log("Mongo DB connected");
+          app.listen(PORT,()=>{
+            console.log('server running on http://localhost:${PORT}');
+          });
+        })
+        .catch((error)=>{
+          console.error("MongoDB connection failed:",error.message);
+        });
+
+
