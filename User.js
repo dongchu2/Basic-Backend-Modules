1504a76d49c1a115d63fb779456a3d84b84c95ab
Dongchu Xia <dongchuxia@tudelft.nl>
2026-07-23 10:39:19 +0200

    Update User.js

--- User.js
+++ User.js
@@ -5,11 +5,17 @@ const userSchema = new mongoose.Schema({
               unique:true,
               trim:true,
              },
-    password:[type:String,
+    password:{type:String,
               required:true,
               unique:true,
               trim:true,
-              }
+              },
+    status:{type:String,
+            required:true,
+            enum:["pending","approved","rejected"],
+            default:"pending"
+           }
+        
   },{timestamps:true });
 
 module.export = mongoose.model("User",userSchema);
